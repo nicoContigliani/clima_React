74efbff187772b6e318c406ab9f6a853b6ae8597
Nicolas_Contigliani <nico.contigliani@gmail.com>
2020-06-26 09:54:17 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -8,16 +8,37 @@ import { WEATHER_KEY } from './keys';
 
 class App extends Component {
 
+  state = {
+    temperature: '',
+    description: '',
+    humidity: '',
+    wind_speed: '',
+    city: '',
+    country: '',
+    error: null
+  }
+
+
   getWeather = async e => {
     const { city, country } = e.target.elements;
     const cityValue = city.value;
     const countryValue = country.value;
 
     e.preventDefault();
-            const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}&units=metric`;
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}&units=metric`;
     const response = await fetch(API_URL);
     const data = await response.json();
-    console.log(data)
+    
+
+    this.setState({
+      temperature: data.main.temp,
+      description: data.weather[0].description,
+      humidity: data.main.humidity,
+      wind_speed: data.wind.speed,
+      city: data.name,
+      country: data.sys.country,
+      error: null
+    })
 
   }
 
@@ -32,7 +53,7 @@ class App extends Component {
           <div className="row">
             <div className="col-mod-6 mx-auto">
               <WeatherForms getWeather={this.getWeather} />
-              <Weatherinfo />
+              <Weatherinfo {...this.state}/>
             </div>
           </div>
         </div>
